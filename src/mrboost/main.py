d1675a8d26f6338b4edcc5a6a7bc35d72acffd90
gRox167 <525563615@qq.com>
2024-05-01 14:27:52 -0700

    add BlackBoneStationary recon in main.py

--- src/mrboost/main.py
+++ src/mrboost/main.py
@@ -42,5 +42,26 @@ def DynGAVibe(data_dir, output_path, contra_num=20, spokes_per_contra=320):
         torch.save(results, str(output_path))
 
 
+def BlackBoneStationary(data_dir, output_path):
+    # %%
+    raw_data, shape_dict, mdh, twixobj = recon.get_raw_data(data_dir)
+    ga_args = recon.BlackBoneStationaryArgs(
+        shape_dict,
+        mdh,
+        twixobj,
+    )
+    preprocessed_data = recon.preprocess_raw_data(raw_data, ga_args)
+    results = recon.mcnufft_reconstruct(preprocessed_data, ga_args, False)
+
+    output_path = Path(output_path)
+    check_mk_dirs(str(output_path.parent))
+    if output_path.suffix == ".mat":
+        savemat(
+            str(output_path),
+            {"image": results.abs().numpy(force=True)},
+        )
+    elif output_path.suffix == ".pt":
+        torch.save(results, str(output_path))
+        
 if __name__ == "__main__":
     fire.Fire()
